9b5a5e0c7fe0942b6d305af992dc189fd59c493f
CrossedCode <dev@crossedcode.com>
2018-09-02 01:15:35 -0400

    Some more tweaks

--- components/Header.js
+++ components/Header.js
@@ -10,7 +10,8 @@ const Header = () => (
         <h2 className="tagline">My tagline should go here.</h2>
         <a
           target="_blank"
-          href="https://docs.google.com/forms/d/e/1FAIpQLSfQlAo1wXHiJMySdU-h8QMtfoz92aMS9eycEHXB6eRCLh8KHA/viewform"
+          // href="https://docs.google.com/forms/d/e/1FAIpQLSfQlAo1wXHiJMySdU-h8QMtfoz92aMS9eycEHXB6eRCLh8KHA/viewform"
+          href="#"
           className="title__potluck-btn"
         >
           Ask a Potluck Question →
@@ -23,23 +24,23 @@ const Header = () => (
             alt=""
             className="avatar"
           />
-          <h3>Wes Bos</h3>
+          <h3>Edwin Rivera</h3>
           <a
             target="_blank"
-            href="https://twitter.com/wesbos"
+            href="https://twitter.com/EdwinRiveraF"
             className="person__social person__social--twitter"
           >
-            @wesbos
+            @EdwinRiveraF
           </a>
           <p>
-            Full Stack JavaScript Developer. Creator of really good{' '}
-            <a target="_blank" href="https://wesbos.com/courses">
-              web development courses
-            </a>. BBQ enthusiast.
+            Web Developer & Graphic Designer. Creador de{' '}
+            <a target="_blank" href="https://www.anchor.com/mercadeo">
+              Mercadeo Digital Para Todos
+            </a>. Entusiasta del Desarrollo Web.
           </p>
         </div>
 
-        <div className="person">
+        {/* <div className="person">
           <img
             src="https://avatars2.githubusercontent.com/u/669383?s=460&v=4"
             alt=""
@@ -60,7 +61,7 @@ const Header = () => (
               Youtuber
             </a>
           </p>
-        </div>
+        </div> */}
       </div>
     </div>
     <Subscribe />
